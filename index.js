b03feb687544be0b082e32ee05f1c3b8a28c1632
Marion Saru <maghangasaru@gmail.com>
2025-04-11 06:32:24 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const headquarters = 42;
+let pickupLocation = 60;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceToBeCalculated = distanceTravelledInFeet(start, destination);
+
+    if (distanceToBeCalculated <= 400) {
+        return 0;
+    } else if (distanceToBeCalculated > 400 && distanceToBeCalculated <= 2000) {
+        return (distanceToBeCalculated - 400) * 0.02;
+    } else if (distanceToBeCalculated > 2000 && distanceToBeCalculated <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
+
+
+console.log(distanceFromHqInBlocks(70) + " blocks");
+console.log(distanceFromHqInFeet(pickupLocation) + " feet");
+console.log(distanceTravelledInFeet(20, 40) + " feet");
+console.log(calculatesFarePrice(40, 20)); 
+
+console.log(calculatesFarePrice(43, 44)); 
+console.log(calculatesFarePrice(34, 32)); 
+console.log(calculatesFarePrice(50, 58)); 
+console.log(calculatesFarePrice(34, 60)); 
